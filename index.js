f5a7dd765c8259e93f1b62a580550fc24a98e07e
Augustin Michez <augustin.michezgrenier@gmail.com>
2022-12-17 17:20:23 +0100

    Website V0 RESET

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-function redirect(content) {
-    $("#pagecontent").load(content)
-}
-
-$(document).ready(function(){
-    //define the scroll variable
-    var scroll_pos = 0;
-    $(document).scroll(function() {
-        scroll_pos = $(this).scrollTop();
-        // If the scroll is higher than 48, there is a shadow effect added
-        if(scroll_pos > 48) {
-            $("#navtitletitle").css('box-shadow', '0px 0px 10px black');
-        } else {
-            $("#navtitletitle").css('box-shadow', 'none');
-        }
-        console.log(scroll_pos)
-    });
-});
-
-function daymode() {
-    $('.day').hide();
-    $('.night').show();
-    $('body').css('background-color', 'white')
-    $('.n').css('color', 'black')
-}
-
-function nightmode() {
-    $('.day').show();
-    $('.night').hide();
-    $('body').css('background-color', '#2e303e')
-    $('.n').css('color', 'white')
+function redirect(e) {
+    $("#pagecontent").load(e);
 }
